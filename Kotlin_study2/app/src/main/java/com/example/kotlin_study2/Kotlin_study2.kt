791d715fd53631761614af8d45be33a84794b012
chungHee Han <chunghuihan9gmail.com>
2022-09-21 21:06:06 +0900

    fourth commit

--- Kotlin_study2/app/src/main/java/com/example/kotlin_study2/Kotlin_study2.kt
+++ Kotlin_study2/app/src/main/java/com/example/kotlin_study2/Kotlin_study2.kt
@@ -7,8 +7,9 @@ package com.example.kotlin_study2
 //람다식 기본 정의
 // val lamdaname : type = {argmentlist -> body}
 //매개 변수 o 리턴 o
-fun sqare(number : Int){
+fun sqare(number : Int): Int {
     val squarenumber = number*number
+    return squarenumber
 }
 val squareLambda :(Int)->(Int) = {
         number -> number*number
@@ -16,15 +17,39 @@ val squareLambda :(Int)->(Int) = {
 var nameage ={
         name : String , age : Int -> "my name is ${name}, age is ${age}"
 }
+
 //매개 변수 X 리턴값 x
 fun someFunction(){
     println("someFunction 함수를 호출하였습니다")
 }
-
+// someFunction() => someLamda
 //매개 변수 x 리린턴값 x
 val someLambda : () -> Unit = {
     println("someLambda가 호출되었습니다")
 }
+//매개 변수 o 리턴값 x
+val someLambdaWithParam : (String) -> Unit = {
+    userImnput -> println("$userImnput")
+}
+
+//매개 변수 2개
+val someLambdaWithParams : (String,Int) -> Unit = {
+        userImnput, num -> println("$userImnput")
+}
+//매개 변수를 사용하지 않을 시 '_' 로 처리
+//val someLamdaWithParams : (String,Int) -> Unit = {
+//        userImnput,_ -> println("$userImnput")
+//}
+
+//함수의 매개변수로 람다를 사용 시
+fun someFunctionWithLambda(completion: () -> Unit){
+    completion()
+}
+
+fun someFunctionWithParamAndLambda(myNumber: Int, completion: (String) -> Unit){
+    completion("myNumber는 $myNumber")
+}
+
 
 fun main(){
     println(squareLambda(12))
@@ -32,6 +57,17 @@ fun main(){
 
     someFunction()
     someLambda.invoke()
+    someLambdaWithParam("호호호")
+    someLambdaWithParams("호호호",1)
+
+    someFunctionWithLambda(completion ={
+        print("람다를 매개 변수로 사용하겼습니다.")
+    })
+
+    someFunctionWithParamAndLambda(300,completion ={
+        print("받은 $it 입니다.")
+    })
+
 
     val a = "han said"
     val b = "kim said"
