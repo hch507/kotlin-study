62c4f0ddb75cab829b246543ab1e12fc19f20143
chung <chunghuihan9@gmail.com>
2022-12-18 17:06:21 +0900

    second commit

--- jetpack_nav/app/src/main/java/com/example/jetpack_nav/MainActivity.kt
+++ jetpack_nav/app/src/main/java/com/example/jetpack_nav/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.jetpack_nav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.jetpack_nav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mbinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mbinding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(mbinding.MyBottomNav,navController)
     }
 }
